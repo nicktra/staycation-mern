34b4bcabb1c4850fea1cc000109db4ca30b838e0
baloh <balohbaloh@yahoo.co.id>
2021-05-05 21:58:52 +0700

    Fix Header Component

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import Header from "parts/IconText";
+import Header from "parts/Header";
 
 export default class LandingPage extends Component {
   render() {
