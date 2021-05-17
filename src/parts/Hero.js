f9437621b04b3e69d2c5b6610d849647939a3776
baloh <balohbaloh@yahoo.co.id>
2021-05-17 10:19:01 +0700

    Styling Hero Component

--- src/parts/Hero.js
+++ src/parts/Hero.js
@@ -19,11 +19,11 @@ export default function Hero(props) {
     <section className="container pt-4">
       <div className="row align-items-center">
         <div className="col-auto pr-5" style={{ width: 530 }}>
-          <h1 className="h2 font-weight-bold line-height-1 mb-3">
+          <h1 className="font-weight-bold line-height-1 mb-3">
             Forget Busy Work, <br />
             Start Next Vacation
           </h1>
-          <p className="mb-5 font-weight-light text-gray-500 w-75">
+          <p className="mb-4 font-weight-light text-gray-500 w-75" style={{ lineHeight: "170%" }}>
             We provide what you need to enjoy your holiday with family. Time to
             make another memorable moments.
           </p>
@@ -36,7 +36,7 @@ export default function Hero(props) {
             Show Me Now
           </Button>
 
-          <div className="row mt-5">
+          <div className="row" style={{ marginTop: 80 }}>
             <div className="col-auto" style={{ marginRight: 35 }}>
               <img
                 width="36"
