fecb340bd95424286e44af4319cade5234e833a1
baloh <balohbaloh@yahoo.co.id>
2021-05-28 19:08:44 +0700

    Finished Component Categories

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -3,6 +3,7 @@ import Header from "parts/Header";
 import landingPage from "json/landingPage.json";
 import Hero from "parts/Hero";
 import MostPicked from "parts/MostPicked";
+import Categories from "parts/Categories";
 
 export default class LandingPage extends Component {
   constructor(props) {
@@ -18,6 +19,9 @@ export default class LandingPage extends Component {
         <MostPicked
           refMostPicked={this.refMostPicked}
           data={landingPage.mostPicked}
+        />
+        <Categories
+          data={landingPage.categories}
         />
       </>
     );
