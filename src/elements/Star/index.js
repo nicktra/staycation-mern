c7cd29b987e8fed791dfc77912e23e57887b866d
baloh <balohbaloh@yahoo.co.id>
2021-05-30 19:06:13 +0700

    Styling Component Star

--- src/elements/Star/index.js
+++ src/elements/Star/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import propTypes from "prop-types";
+import "./index.scss";
 
 export default function Star({ className, value, height, width, spacing }) {
   const decimals = Number(value) % 1;
@@ -11,7 +12,12 @@ export default function Star({ className, value, height, width, spacing }) {
       <div
         className="star"
         key={`star-${index}`}
-        style={{ left: index * width, width: width, marginRight: spacing }}
+        style={{
+          left: index * width,
+          height: height,
+          width: width,
+          marginRight: spacing,
+        }}
       ></div>
     );
   }
@@ -19,8 +25,12 @@ export default function Star({ className, value, height, width, spacing }) {
     star.push(
       <div
         className="star"
-        key={`star-${index}`}
-        style={{ left: leftPos, width: width - spacing }}
+        key={`starWithDecimal}`}
+        style={{
+          left: leftPos,
+          height: height,
+          width: decimals * width - spacing,
+        }}
       ></div>
     );
 
@@ -30,7 +40,12 @@ export default function Star({ className, value, height, width, spacing }) {
       <div
         className="star placeholder"
         key={`starPlaceholder-${index}`}
-        style={{ left: index * width, width: width, marginRight: spacing }}
+        style={{
+          left: index * width,
+          height: height,
+          width: width,
+          marginRight: spacing,
+        }}
       ></div>
     );
   }
