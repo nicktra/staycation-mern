9df9c661a0d0eec93b96f298310081ea46997414
baloh <balohbaloh@yahoo.co.id>
2021-06-07 19:34:25 +0700

    Add testing InputDate

--- src/elements/Form/InputDate/index.js
+++ src/elements/Form/InputDate/index.js
@@ -7,7 +7,7 @@ import "react-date-range/dist/styles.css"; // main css file
 import "react-date-range/dist/theme/default.css"; // theme css file
 
 import formatDate from "utils/formatDate";
-import iconCalendar from "assets/images/ic_calendar.svg";
+import iconCalendar from "assets/images/icons/icon-calendar.svg";
 
 export default function Date(props) {
   const { value, placeholder, name } = props;
