c636ddb3ee69688c54fa24779f74fb189d34dd20
baloh <balohbaloh@yahoo.co.id>
2021-12-11 17:13:45 +0700

    Integration Checkout Page

--- src/pages/Checkout.js
+++ src/pages/Checkout.js
@@ -17,6 +17,8 @@ import Completed from "parts/Checkout/Completed";
 
 import ItemDetails from "json/itemDetails.json";
 
+import { submitBooking } from "store/actions/checkout";
+
 class Checkout extends Component {
   state = {
     data: {
@@ -43,9 +45,38 @@ class Checkout extends Component {
     window.scroll(0, 0);
   }
 
+  _Submit = (nextStep) => {
+    const { data } = this.state;
+    const { checkout } = this.props;
+
+    const payload = new FormData();
+    payload.append("firstName", data.firstName);
+    payload.append("lastName", data.lastName);
+    payload.append("email", data.email);
+    payload.append("phoneNumber", data.phone);
+    payload.append("idItem", checkout._id);
+    payload.append("duration", checkout.duration);
+    payload.append("bookingStartDate", checkout.date.startDate);
+    payload.append("bookingEndDate", checkout.date.endDate);
+    payload.append("accountHolder", data.bankHolder);
+    payload.append("bankFrom", data.bankName);
+    payload.append("image", data.proofPayment[0]);
+    // payload.append("bankId", checkout.bankId);
+
+    this.props
+      .submitBooking(payload)
+      .then(() => {
+        nextStep();
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   render() {
     const { data } = this.state;
     const { checkout, page } = this.props;
+    console.log(page, data);
 
     if (!checkout)
       return (
@@ -166,7 +197,7 @@ class Checkout extends Component {
                             isBlock
                             isPrimary
                             hasShadow
-                            onClick={nextStep}
+                            onClick={() => this._Submit(nextStep)}
                           >
                             Continue to Book
                           </Button>
@@ -212,4 +243,4 @@ const mapStateToProps = (state) => ({
   page: state.page,
 });
 
-export default connect(mapStateToProps)(Checkout);
+export default connect(mapStateToProps, { submitBooking })(Checkout);
